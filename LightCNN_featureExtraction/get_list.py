52b9d7e79964aa3b6ab9abe048a4f113b080cf03
garg-7 <57702625+garg-7@users.noreply.github.com>
2020-04-24 22:02:55 +0530

    added get_list.py
    Creates a list of files present in the data folder into a list.txt file.

---
+++ LightCNN_featureExtraction/get_list.py
@@ -0,0 +1,14 @@
+import os
+
+f = open('data/list.txt','w')
+
+name_list = list()
+for (dirpath, dirname, filename) in os.walk('data/'):
+    for files in filename:
+        name_list.append(files)
+
+name_list = sorted(name_list)
+for files in name_list:
+    f.write(files+os.linesep)
+f.close()
+
