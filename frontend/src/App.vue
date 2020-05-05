19875560a6ded5255c62dd1f048babf61b12f4f3
Tanuj22 <agarwal.5@iitj.ac.in>
2020-05-05 16:37:58 +0530

    initialized frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  div(id="app")
+    router-view
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,10 @@
+<template lang="pug">
+  div(class="home" align="center")
+    img(alt="Devlup Labs logo" src="../assets/logo.jpeg")
+</template>
+
+<script>
+export default {
+  name: "Home"
+};
+</script>
