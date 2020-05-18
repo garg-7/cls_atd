b231a521386954667deccd28b7616aecd270d19f
Tanuj22 <agarwal.5@iitj.ac.in>
2020-05-18 23:46:22 +0530

    add fix json response

--- src/api/LightCNN/cossim.py
+++ src/api/LightCNN/cossim.py
@@ -3,52 +3,61 @@ from numpy.linalg import norm
 import json
 import numpy as np
 
-sim = dict()    # dictionary that stores:
-                # {
-                #   test_face1:[(ref_face1, similarity_with_it),(ref_face2, similarity_with_it)...],
-                #   test_face2:[(ref_face1, similarity_with_it),(ref_face2, similarity_with_it)...] 
-                # }
+ans = []
+
+sim = dict()  # dictionary that stores:
+# {
+#   test_face1:[(ref_face1, similarity_with_it),(ref_face2, similarity_with_it)...],
+#   test_face2:[(ref_face1, similarity_with_it),(ref_face2, similarity_with_it)...]
+# }
 test_faces = list()  # list of all the test faces
 
-test_data_list = open('list.txt','r') # open the file that has the list of test images
+test_data_list = open('list.txt', 'r')  # open the file that has the list of test images
 
 for test_img_name in test_data_list:
-    test_img_name = test_img_name.replace('.jpg\n', '.feat')    # the feature file would have a different extension so change it
-    
-    test_faces.append(test_img_name.replace('.feat','')) # append the filename of the test face
+    test_img_name = test_img_name.replace('.jpg\n',
+                                          '.feat')  # the feature file would have a different extension so change it
 
-    temp = list()       #   temporary list to store the various (ref_face, similarity) pairs for each test_face
+    test_faces.append(test_img_name.replace('.feat', ''))  # append the filename of the test face
 
-    test_feat_file = open('save_path/' + test_img_name, 'rb')   # opening the feature file of the test face
-    
-    test_feat = np.fromfile(test_feat_file, dtype = '<f', count = -1) #loading the features of the test img
-    
-    maxsim = 0      # storing the maximum similarity
-    maxsim_name = 'NULL'    # the reference img with which max similarity occurs
-    
-    ref_faces_list = open('liststd.txt','r')
+    temp = list()  # temporary list to store the various (ref_face, similarity) pairs for each test_face
+
+    test_feat_file = open('save_path/' + test_img_name, 'rb')  # opening the feature file of the test face
+
+    test_feat = np.fromfile(test_feat_file, dtype='<f', count=-1)  # loading the features of the test img
+
+    maxsim = 0  # storing the maximum similarity
+    maxsim_name = 'NULL'  # the reference img with which max similarity occurs
+
+    ref_faces_list = open('liststd.txt', 'r')
     for ref_img_name in ref_faces_list:
 
-        ref_img_name = ref_img_name.replace('.jpg\n','.feat') # the feature file would have a different extension so change it
-        
-        ref_feat_file = open('save_path/standard_features/'+ ref_img_name,'rb') # opening the feature file of the ref face
-        
-        ref_feat = np.fromfile(ref_feat_file, dtype = '<f', count = -1)  # loading the features of the ref img
-        
+        ref_img_name = ref_img_name.replace('.jpg\n',
+                                            '.feat')  # the feature file would have a different extension so change it
+
+        ref_feat_file = open('save_path/standard_features/' + ref_img_name,
+                             'rb')  # opening the feature file of the ref face
+
+        ref_feat = np.fromfile(ref_feat_file, dtype='<f', count=-1)  # loading the features of the ref img
+
         cos_sim = abs(dot(test_feat, ref_feat) / (norm(test_feat) * norm(ref_feat)))
-        
-        temp.append((ref_img_name.replace('.feat',''),float(cos_sim)))
-        
+
+        temp.append((ref_img_name.replace('.feat', ''), float(cos_sim)))
+
         if cos_sim > maxsim:
             maxsim = cos_sim
             maxsim_name = ref_img_name
-    
+
     ref_faces_list.close()
-    
-    sim[test_img_name.replace('.feat','')] = temp
-    print(test_img_name.replace('.feat','')+" - " + maxsim_name.replace('f.feat','') + "| Score :", maxsim)
+
+    sim[test_img_name.replace('.feat', '')] = temp
+    print(test_img_name.replace('.feat', '') + " - " + maxsim_name.replace('f.feat', '') + "| Score :", maxsim)
+    ans.append({
+        "roll_no": maxsim_name.replace('f.feat', ''),
+        "attendance": 'Present'
+    })
     with open('scores.json', 'w') as handle:
-        json.dump(maxsim_name.replace('f.feat',''), handle)
+        json.dump(sim, handle)
 
 # print("\n\n----Normal execution ended----\n\n")
 
