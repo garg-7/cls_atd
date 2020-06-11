d28ac29a12f5ee5f5b62478749a64169f9f742c8
Tanuj22 <agarwal.5@iitj.ac.in>
2020-06-11 18:07:04 +0530

    add models

--- src/api/models.py
+++ src/api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    roll_no = models.CharField(max_length=16, primary_key=True)
+
+
+class Attendance(models.Model):
+    date = models.DateField(unique_for_date=True, auto_now_add=True)
+    status = models.ManyToManyField(Student, through='AttendanceStatus',
+                                    through_fields=('attendance', 'student'))
+
+
+class AttendanceStatus(models.Model):
+    STATUS_CHOICES = (
+        ('1', 'PRESENT'),
+        ('2', 'ABSENT')
+    )
+
+    attendance = models.ForeignKey(Attendance, models.CASCADE)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    status = models.CharField(choices=STATUS_CHOICES, max_length=2)
