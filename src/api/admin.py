5bb63ee0e851a18f782eb6f072cdad9f9dd0be99
Tanuj22 <agarwal.5@iitj.ac.in>
2020-06-11 18:23:24 +0530

    add admin

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Student, Attendance, AttendanceStatus
 
-# Register your models here.
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Student
+        fields = '__all__'
+
+
+class AttendanceStatusInline(admin.StackedInline):
+    model = AttendanceStatus
+
+
+@admin.register(Attendance)
+class AttendanceAdmin(admin.ModelAdmin):
+    inlines = (AttendanceStatusInline, )
+
+    class Meta:
+        model = Attendance
+        fields = '__all__'
--- src/api/models.py
+++ src/api/models.py
@@ -4,11 +4,17 @@ from django.db import models
 class Student(models.Model):
     roll_no = models.CharField(max_length=16, primary_key=True)
 
+    def __str__(self):
+        return self.roll_no
+
 
 class Attendance(models.Model):
     date = models.DateField(unique_for_date=True, auto_now_add=True)
     status = models.ManyToManyField(Student, through='AttendanceStatus',
                                     through_fields=('attendance', 'student'))
+
+    def __str__(self):
+        return self.date
 
 
 class AttendanceStatus(models.Model):
@@ -20,3 +26,7 @@ class AttendanceStatus(models.Model):
     attendance = models.ForeignKey(Attendance, models.CASCADE)
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     status = models.CharField(choices=STATUS_CHOICES, max_length=2)
+
+    def __str__(self):
+        return self.student
+
