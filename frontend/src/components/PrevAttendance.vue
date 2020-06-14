fdb32a94f60bc51752af0a8f8875cc02880bf56a
Tanuj22 <agarwal.5@iitj.ac.in>
2020-06-14 20:23:30 +0530

    initialized prev attendance page

---
+++ frontend/src/components/PrevAttendance.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  h1 Hello
+</template>
+
+<script>
+    export default {
+        name: "PrevAttendance"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,14 +1,28 @@
 <template lang="pug">
   div(class="home" align="center")
-    ImageUpload
+    div(v-if="step===0")
+      h1.ma-12 Welcome to Face Attendance App :)
+      v-btn.ma-12.white--text( @click = "step=1" color="blue") Previous Attendance
+      v-btn.ma-12.white--text(@click = "step=2" color="blue") New Attendance
+    div(v-if="step===1")
+      PrevAttendance
+    div(v-if="step===2")
+      ImageUpload
 </template>
 
 <script>
 import ImageUpload from "@/components/ImageUpload.vue"
+import PrevAttendance from "../components/PrevAttendance";
     export default {
         name: "Home",
         components:{
-            ImageUpload
+            ImageUpload,
+            PrevAttendance
+        },
+        data() {
+            return{
+              step: 0
+          }
         }
     };
 </script>
